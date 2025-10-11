45c033a0dc5ec99f000a4b847c1e6ae24f5dc8c2
duyv0-admin <duyvo2k59a@gmail.com>
2025-10-11 13:31:54 +0700

    add

--- quanlisinhvien/Form1.Designer.cs
+++ quanlisinhvien/Form1.Designer.cs
@@ -43,17 +43,19 @@
             this.lblusername.AutoSize = true;
             this.lblusername.BackColor = System.Drawing.Color.Transparent;
             this.lblusername.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.lblusername.Location = new System.Drawing.Point(105, 158);
+            this.lblusername.Location = new System.Drawing.Point(79, 128);
+            this.lblusername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblusername.Name = "lblusername";
-            this.lblusername.Size = new System.Drawing.Size(70, 16);
+            this.lblusername.Size = new System.Drawing.Size(55, 13);
             this.lblusername.TabIndex = 0;
             this.lblusername.Text = "Username";
             // 
             // txtusername
             // 
-            this.txtusername.Location = new System.Drawing.Point(209, 158);
+            this.txtusername.Location = new System.Drawing.Point(157, 128);
+            this.txtusername.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtusername.Name = "txtusername";
-            this.txtusername.Size = new System.Drawing.Size(205, 22);
+            this.txtusername.Size = new System.Drawing.Size(155, 20);
             this.txtusername.TabIndex = 1;
             // 
             // lblpassword
@@ -61,26 +63,29 @@
             this.lblpassword.AutoSize = true;
             this.lblpassword.BackColor = System.Drawing.Color.Transparent;
             this.lblpassword.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.lblpassword.Location = new System.Drawing.Point(105, 223);
+            this.lblpassword.Location = new System.Drawing.Point(79, 181);
+            this.lblpassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblpassword.Name = "lblpassword";
-            this.lblpassword.Size = new System.Drawing.Size(67, 16);
+            this.lblpassword.Size = new System.Drawing.Size(53, 13);
             this.lblpassword.TabIndex = 0;
             this.lblpassword.Text = "Password";
             // 
             // txtpassword
             // 
-            this.txtpassword.Location = new System.Drawing.Point(209, 220);
+            this.txtpassword.Location = new System.Drawing.Point(157, 179);
+            this.txtpassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtpassword.Name = "txtpassword";
-            this.txtpassword.Size = new System.Drawing.Size(205, 22);
+            this.txtpassword.Size = new System.Drawing.Size(155, 20);
             this.txtpassword.TabIndex = 2;
             // 
             // btnsignin
             // 
             this.btnsignin.BackColor = System.Drawing.SystemColors.Info;
             this.btnsignin.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnsignin.Location = new System.Drawing.Point(226, 278);
+            this.btnsignin.Location = new System.Drawing.Point(170, 226);
+            this.btnsignin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnsignin.Name = "btnsignin";
-            this.btnsignin.Size = new System.Drawing.Size(75, 23);
+            this.btnsignin.Size = new System.Drawing.Size(56, 19);
             this.btnsignin.TabIndex = 2;
             this.btnsignin.Text = "Sign in";
             this.btnsignin.UseVisualStyleBackColor = false;
@@ -89,9 +94,10 @@
             // btncancel
             // 
             this.btncancel.BackColor = System.Drawing.SystemColors.Info;
-            this.btncancel.Location = new System.Drawing.Point(339, 278);
+            this.btncancel.Location = new System.Drawing.Point(254, 226);
+            this.btncancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btncancel.Name = "btncancel";
-            this.btncancel.Size = new System.Drawing.Size(75, 23);
+            this.btncancel.Size = new System.Drawing.Size(56, 19);
             this.btncancel.TabIndex = 2;
             this.btncancel.Text = "Cancel";
             this.btncancel.UseVisualStyleBackColor = false;
@@ -100,19 +106,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(366, 81);
+            this.label1.Location = new System.Drawing.Point(274, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 3;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(617, 415);
+            this.ClientSize = new System.Drawing.Size(463, 337);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btncancel);
             this.Controls.Add(this.btnsignin);
@@ -120,8 +127,10 @@
             this.Controls.Add(this.txtusername);
             this.Controls.Add(this.lblpassword);
             this.Controls.Add(this.lblusername);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Đăng Nhập";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
