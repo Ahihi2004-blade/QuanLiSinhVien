e0c46032d364b0c61f107b193e22e5d392ab0b12
Min00103 <tranvanluongzh@gmail.com>
2025-10-21 18:02:23 +0700

    da them

--- quanlisinhvien/QuanLiDiem.cs
+++ quanlisinhvien/QuanLiDiem.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace quanlisinhvien
 {
@@ -18,7 +19,24 @@ namespace quanlisinhvien
             InitializeComponent();
         }
 
-       
+        private void QuanLiDiem_Load(object sender, EventArgs e)
+        {
+            string[] gradingScale = { "A", "B+", "B", "C+", "C", "D+", "D", "F" };
+            cmbxephang.Items.AddRange(gradingScale);
+            comboBox2.Items.Add("Đặt mục tiêu");
+            comboBox2.Items.Add("Bằng khá");
+            comboBox2.Items.Add("Bằng giỏi");
+            comboBox2.Items.Add("Bằng xuất sắc");
+
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedGoal = comboBox2.SelectedItem.ToString();
+           
+
+        }
     }
 }
 
